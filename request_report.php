41172acd137a923fc24e2cc0e463653e4ee23554
halifafaye1 <64530868+halifafaye1@users.noreply.github.com>
2020-05-26 14:27:11 +0000

    Update request_report.php
    Co-Authored-By: jallow101 <jallow101@users.noreply.github.com>

--- request_report.php
+++ request_report.php
@@ -180,6 +180,7 @@ include('connection/connection.php');
                         <th>Reference #</th>
                         <th>Date/Time</th>
                         <th>Status</th>
+                        <th>renewal</th>
                       </tr>
 
 
@@ -242,6 +243,34 @@ include('connection/connection.php');
                   <td><?php echo $row['file_ref_no']; ?></td>
                   <td><?php echo $row['date_time']; ?></td>
                   <td><?php echo $row['approval']; ?></td>
+                  <td> 
+                  <a href="#delete<?php echo $row['id']; ?>" data-toggle="modal" class="btn btn-danger"><span class="glyphicon glyphicon-plus"></span> Renew</a>
+							<!-- Renewal -->
+						    <div class="modal fade" id="delete<?php echo $row['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						        <div class="modal-dialog">
+						            <div class="modal-content">
+						                <div class="modal-header">
+						                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						                    <center><h4 class="modal-title" id="myModalLabel">Renew</h4></center>
+						                </div>
+						                <div class="modal-body">
+										<?php
+											$del=mysqli_query($conn,"select * from person where id='".$row['id']."'");
+											$drow=mysqli_fetch_array($del);
+										?>
+										<div class="container-fluid">
+											<h5><center>Firstname: <strong><?php echo $drow['name']; ?></strong></center></h5>
+						                </div>
+										</div>
+						                <div class="modal-footer">
+						                    <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+						                    <a href="deleteperson.php?id=<?php echo $row['id']; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+						                </div>
+
+						            </div>
+						        </div>
+						    </div>
+                   </td>
 
               </tr>
 
@@ -292,7 +321,31 @@ include('connection/connection.php');
           </section><!-- /.content -->
         </div><!-- /.container -->
       </div><!-- /.content-wrapper -->
+	<!-- Delete -->
+  <div class="modal fade" id="delete<?php echo $row['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						        <div class="modal-dialog">
+						            <div class="modal-content">
+						                <div class="modal-header">
+						                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						                    <center><h4 class="modal-title" id="myModalLabel">Delete</h4></center>
+						                </div>
+						                <div class="modal-body">
+										<?php
+											$del=mysqli_query($conn,"select * from renew where id='".$row['id']."'");
+											$drow=mysqli_fetch_array($del);
+										?>
+										<div class="container-fluid">
+											<h5><center>Firstname: <strong><?php echo $drow['name']; ?></strong></center></h5>
+						                </div>
+										</div>
+						                <div class="modal-footer">
+						                    <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+						                    <a href="deleteperson.php?id=<?php echo $row['id']; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+						                </div>
 
+						            </div>
+						        </div>
+						    </div>
     </div><!-- ./wrapper -->
 
 
