e4cfc89652a0b08cf278811a03892cde5afeb741
halifafaye1 <64530868+halifafaye1@users.noreply.github.com>
2020-06-01 19:12:13 +0000

    charges

--- Ajax.php
+++ Ajax.php
@@ -2,11 +2,11 @@
 
   require 'connection/connection.php';
 if (isset($_POST['name'])) {
-    $qry = "select * from `school` where id=".$_POST['name'];
+    $qry = "select * from `sch_list` where sid=".$_POST['name'];
     $rec = mysqli_query($conn,$qry);
 
         while ($res = mysqli_fetch_array($rec)) {
-            echo $res['sch_code']."|".$res['district']."|".$res['cluster']."|".$res['region'];
+            echo $res['sch_type']."|". $res['sch_code']."|".$res['district']."|" .$res['cluster']."|".$res['region']."|".$res['ward'] .$res['settlement'];
 
     }
 }
