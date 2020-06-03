8a85d7d8286bfb0bd3d29f70d34afcac86fbdf6d
jallow101 <jallow101@users.noreply.github.com>
2020-06-03 12:16:43 +0000

    Update deleteRequest.php

--- deleteRequest.php
+++ deleteRequest.php
@@ -2,6 +2,8 @@
 	include('connection/connection.php');
 	$id=$_GET['id'];
 	mysqli_query($conn,"delete from request where id='$id'");
+	mysqli_query($conn,"delete from report where request_id='$id'");
+	mysqli_query($conn,"delete from renewal where request_id='$id'");
 	header('location:request.php');
 
 ?>
